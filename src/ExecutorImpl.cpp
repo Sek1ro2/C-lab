4656ab00b52eac54c137c24da91b96841f205a67
Sek1ro2 <189743224+Sek1ro2@users.noreply.github.com>
2025-11-15 15:51:11 +0800

    feat:Add test cases for initial and default poses

---
+++ src/ExecutorImpl.cpp
@@ -0,0 +1,20 @@
+#include "ExecutorImpl.hpp"
+#include <new>
+
+namespace adas
+{
+    // 并没有初始化ExecutorImpl的pose成员变量
+    ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept {}
+
+    // Query方法
+    Pose ExecutorImpl::Query(void) const noexcept
+    {
+        return pose;
+    }
+
+    // NewExecutor方法
+    Executor *Executor::NewExecutor(const Pose &pose) noexcept
+    {
+        return new (std::nothrow) ExecutorImpl(pose); // c++17
+    }
+}
