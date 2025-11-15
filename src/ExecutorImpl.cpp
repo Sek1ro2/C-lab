43fb2d4f41e734c50a300421ee9ebd228a9f44f8
Sek1ro2 <189743224+Sek1ro2@users.noreply.github.com>
2025-11-15 16:10:20 +0800

    test:Pass move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -17,7 +17,28 @@ namespace adas
     {
         return new (std::nothrow) ExecutorImpl(pose); // c++17
     }
-    void ExecutorImpl::Execute(const std::string &commands) noexcept{
-        //
+    void ExecutorImpl::Execute(const std::string &commands) noexcept {
+        for (const auto cmd : commands)
+        {
+            if (cmd == 'M')
+            {
+                if (pose.heading == 'E')
+                {
+                    ++pose.x;
+                }
+                else if (pose.heading == 'W')
+                {
+                    --pose.x;
+                }
+                else if (pose.heading == 'N')
+                {
+                    ++pose.y;
+                }
+                else if (pose.heading == 'S')
+                {
+                    --pose.y;
+                }
+            }
+        }
     }
 }
